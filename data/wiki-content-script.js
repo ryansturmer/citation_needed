b2296f1c64c48bd90f37460b7124bae95da2be19
Ryan Sturmer <ryansturmer@gmail.com>
2012-08-11 21:03:30 -0400

    Some serious cleanup.

--- data/wiki-content-script.js
+++ data/wiki-content-script.js
@@ -1,3 +1,2 @@
 console.log("Executing the content script and emitting load event " + [document.referrer, document.location.href]);
 self.port.emit("load", [document.referrer, document.location.href]);
-
