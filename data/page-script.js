d552ab8a474f44c42329b545f186e0558ece814b
Ryan Sturmer <ryansturmer@gmail.com>
2012-08-20 08:55:50 -0400

    Not much progress. Moved to sdk-1.9, Got the content script to communicate with the page

---
+++ data/page-script.js
@@ -0,0 +1,14 @@
+console.log("Executing page-script.js");
+
+// When a tab is focused
+self.port.on("focus", function(msg) {
+
+
+});
+
+// When a page is loaded
+self.port.on("load", function(msg) {
+
+
+});
+
