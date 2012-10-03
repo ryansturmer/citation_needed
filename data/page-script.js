678b887a522442a55eaf4ac21db48115c1beba01
Ryan Sturmer <ryansturmer@gmail.com>
2012-10-02 21:07:31 -0400

    Graph persistence works properly.

--- data/page-script.js
+++ data/page-script.js
@@ -1,16 +1,16 @@
-//console.log("Executing page-script.js");
-
 function dispatch_event(event_name, data) {
     var event = document.createEvent('CustomEvent');
     event.initCustomEvent(event_name, true, true, data);
     document.documentElement.dispatchEvent(event);
 }
 
+self.port.on("entry", function(msg) {
+    dispatch_event('page-entry', msg);
+});
 // When a tab is focused
 self.port.on("focus", function(msg) {
     dispatch_event('page-focus', msg);
 });
-
 
 // When a page is loaded
 self.port.on("load", function(msg) {
--- data/wiki-content-script.js
+++ data/wiki-content-script.js
@@ -1,2 +1,1 @@
-//console.log("Executing the content script and emitting load event " + [document.referrer, document.location.href]);
 self.port.emit("load", [document.referrer, document.location.href]);
