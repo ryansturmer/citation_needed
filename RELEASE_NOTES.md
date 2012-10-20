53426b3007a8d8c6d96adfcea2913d56af354cd4
Ryan Sturmer <ryansturmer@gmail.com>
2012-10-20 14:21:21 -0400

    Fixed the clear button.

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+1.0.9
+ * Added release notes
+ * Fixed the clear button so it actually clears the UI
--- bin/citation_needed.js
+++ bin/citation_needed.js
@@ -1,1 +1,1 @@
-var xpi_hash = {"hash": "sha1:98bb272fd391924d37b2e4c1324143846db3d330"};
+var xpi_hash = {"hash": "sha1:d05151b854111c4f42e93352ec683379a3532c46"};
