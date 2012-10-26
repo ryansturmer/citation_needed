966da023f8a312c866532bd1ab1891ee186703bd
Ryan Sturmer <ryansturmer@gmail.com>
2012-10-25 23:29:47 -0400

    Histories saved across browser session.

--- data/page-script.js
+++ data/page-script.js
@@ -1,3 +1,8 @@
+/*
+ * This is the page script, which runs for index.htm
+ * Events dispatched from here land in the index.htm universe
+ * Events caught here (port.on) came from the main.js universe
+ */
 function dispatch_event(event_name, data) {
     var event = document.createEvent('CustomEvent');
     event.initCustomEvent(event_name, true, true, data);
@@ -18,6 +23,9 @@ self.port.on("load", function(msg) {
 });
 
 self.port.on("activate", function(msg) {
-
+    self.port.on
 });
 
+document.documentElement.addEventListener("clear", function(event) {
+    self.port.emit("clear", event.detail);
+}, false);
